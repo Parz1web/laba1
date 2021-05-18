93cac6ad615ac6f3ece408ae196ff2738aa72c5f
Ivan <example@gmail.com>
2021-05-18 19:51:41 +0300

    Добавьте файлы проекта.

---
+++ lab1/lab1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+double y(double);
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    double n;
+    cout << "Введите число: ";
+    cin >> n;
+
+    cout << y(n) <<endl;
+
+    return 0;
+}
+
+double y(double n)
+{
+    if (n == 1)
+        return n;
+    else
+        return sqrt(sqrt(n) + y(n - 1));
+}
